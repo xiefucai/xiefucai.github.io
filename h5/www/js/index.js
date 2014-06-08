3217cc28f669628ae6230a86385299cd742ab73b
xiefucai <laozi12345@126.com>
2014-06-08 21:38:46 +0800

    a

--- h5/www/js/index.js
+++ h5/www/js/index.js
@@ -1,8 +1,49 @@
 ﻿;(function($) {
 	var slideElem = $('#sliders'),
+		form = $('#form')[0],
 	pageY = 0,
 	pageY2 = 0,
 	scrollTop = 0,
+	turnPage = function(i){
+		var t = document.body.scrollTop,
+			h = document.body.clientHeight,
+			t2 = i * h,
+			d = Math.floor((t2 - t)/10),
+			times = 0,
+			timer = 0;
+		document.body.scrollTop = (t2-t)%10 + t;
+		timer = setInterval(function(){
+			if (times < 10){
+				document.body.scrollTop = document.body.scrollTop + d;
+				times++;
+			}else{
+				clearInterval(timer);
+				document.body.scrollTop = t2;
+			}
+		},10);
+	},
+	isTelNum = function(val){
+		if (new RegExp(/[^\d]/g).test(c)) {
+			return false;
+		}
+		if (val.length == 11) {
+			if (new RegExp(/^1(?:3|5|8)\d{9}$/).test(val)) {
+				return true
+			} else {
+				return false;
+			}
+		} else {
+			if (c.length >= 14) {
+				if (new RegExp(/^008860?9\d{8}$/).test(c)) {
+					return true
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}	
+	},
 	slider = {
 		'index':0,
 		'count':slideElem.find('.page').length,
@@ -42,6 +83,17 @@
 				slider.slideTo(i-1);
 			}
 		}
+	},
+	showResult = function(i,t){
+		var result = $('#result');
+		if (i === 0){
+			result.removeClass('red none').html(t);
+		}else{
+			result.removeClass('none').addClass('red').html(t);
+		}
+		setTimeout(function(){
+			result.addClass('none');
+		},1500);
 	};
 	$('#sliders').bind({
 		'touchstart':function(event){
@@ -59,6 +111,7 @@
 		'touchend':function(event){
 			var d = pageY2 - pageY;
 			if (((slider.count - 1) === slider.index) && (pageY2 - pageY < 0)){
+				turnPage(1);
 				return true;
 			}
 			if (d > 100){
@@ -70,6 +123,68 @@
 			}
 			return false;
 		}
+	});
+	
+	$('#page-share').bind({
+		'touchstart':function(event){
+			pageY = event.touches[0].pageY;
+			scrollTop = document.body.scrollTop;
+		},
+		'touchmove':function(event){
+			pageY2 = event.touches[0].pageY;
+			document.body.scrollTop = scrollTop - (pageY2 - pageY);
+			return false;
+		},
+		'touchend':function(event){
+			var d = pageY2 - pageY;
+			if (d > 50){
+				turnPage(0);
+			}
+			return false;
+		}
+	});
+	
+	$(document).bind({
+		'touchmove':function(){return false;}
+	});
+	
+	$(form).bind({
+		'submit':function(event){
+			var tel = $.trim(form['tel'].value);
+			if ($(form['submit']).hasClass('loading')){
+				return false;
+			}
+			if (!tel){
+				showResult(1,'请输入手机号码');
+			}else if(!isTelNum(tel)){
+				showResult(1,'手机号码格式错误');
+			}else{
+				$(form['submit']).addClass('loading');
+				$.ajax({
+					'url':'/operation/bookAccount',
+					'data':{'mobileNum':tel},
+					'type':'post',
+					'dataType':'json',
+					'success':function(data){
+						if (data && (data.rtn === 1)){
+							showResult(0,'感谢您的申请！<br/>由于申请人数众多，我们将随机分配内测资格。如果申请成功，您会收到应用的内测邀请码和下载链接。谢谢！');
+						}else{
+							showResult(1,d.msg);
+						}
+						$(form['submit']).removeClass('loading');
+					},
+					'error':function(){
+						showResult(1,'哦哦，预约失败了，重试一下吧:)');
+						$(form['submit']).removeClass('loading');
+					}
+				});
+			}
+			return false;
+		}
+	});
+	
+	$('#delbtn').bind('click',function(){
+		form['tel'].value = '';
 	});
 	slider.slideTo(0);
 })(Zepto);
