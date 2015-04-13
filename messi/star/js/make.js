218c24df7a268ec21568e696517a5717ba214597
xiefucai <laozi12345@126.com>
2015-04-13 14:51:20 +0800

    xx

--- messi/star/js/make.js
+++ messi/star/js/make.js
@@ -7,7 +7,7 @@ $(function(){
 		var a = $(event.target);
 		if (a[0].tagName === 'A'){
 			a.addClass('active').siblings().removeClass('active');
-			
+
 			if (a.attr('data-year')){
 				data['year'] = a.attr('data-year');
 			}
@@ -19,7 +19,7 @@ $(function(){
 			}
 		}
 	});
-	
+
 	$('#ok').bind('click',function(){
 		if (!data['year']){
 			alert('请选择出生年份');
@@ -35,9 +35,10 @@ $(function(){
 		}
 		if (data['year'] && data['month'] && data['day']){
 			$('#masker').addClass('none');
+			$('#select').html(data['year']+'年'+('0'+data['month']).slice(-2)+'月'+('0'+data['day']).slice(-2)+'日');
 		}
 	});
-	
+
 	$('#make').bind('click',function(){
 		if (data['year'] && data['month'] && data['day']){
 			location.href = 'upload.html?y='+data['year']+'&m='+data['month']+'&d='+data['day'];
