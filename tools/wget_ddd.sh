0f8cb52fda1206df2cca9a0af41b9f2224438533
xiefucai <laozi12345@126.com>
2015-11-05 15:45:39 +0800

    add wget_ddd.sh

--- tools/wget_ddd.sh
+++ tools/wget_ddd.sh
@@ -1,10 +1,10 @@
 f=/etc/hotplug/usb/udisk_insert
 isListened=`grep 'get_log_when_offline' $f`;
-echo $isListened;
+echo '<span style="color:blue;">'$isListened'</span>';
 
 if [ -n $isListened ];then 
 	echo 'download...'
 	wget http://www.xiefucai.com/tools/ddd.sh -O /thunder/scripts/ddd.sh 2>&1;chmod +x /thunder/scripts/ddd.sh;echo " sh /thunder/scripts/ddd.sh & #get_log_when_offline" >> /etc/hotplug/usb/udisk_insert 2>&1;
 else
-	echo 'done' 
+	echo 'It has been installed days ago!'
 fi
