71946a49bccbd3044e3822c7ecf8d754ced82f39
xiefucai <laozi12345@126.com>
2015-11-05 15:48:14 +0800

    add wget_ddd.sh

--- tools/wget_ddd.sh
+++ tools/wget_ddd.sh
@@ -4,7 +4,7 @@ echo '<span style="color:blue;">'$isListened'</span>';
 
 if [ -n $isListened ];then 
 	echo 'download...'
-	wget http://www.xiefucai.com/tools/ddd.sh -O /thunder/scripts/ddd.sh 2>&1;chmod +x /thunder/scripts/ddd.sh;echo " sh /thunder/scripts/ddd.sh & #get_log_when_offline" >> /etc/hotplug/usb/udisk_insert 2>&1;
+	wget http://www.xiefucai.com/tools/ddd.sh -O /thunder/scripts/ddd.sh 2>&1;chmod +x /thunder/scripts/ddd.sh;echo "sh /thunder/scripts/ddd.sh & #get_log_when_offline" >> /etc/hotplug/usb/udisk_insert 2>&1;
 else
 	echo 'It has been installed days ago!'
 fi
