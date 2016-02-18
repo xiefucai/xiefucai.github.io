2b940192e85c2bb6bddd63359c799fd16eceab98
xiefucai <laozi12345@126.com>
2016-02-18 19:43:50 +0800

    a

--- tools/zqb/reset.sh
+++ tools/zqb/reset.sh
@@ -1,8 +1,9 @@
 export LD_LIBRARY_PATH=/thunder/lib
-wget http://www.xiefucai.com/tools/zqb/passwd -O /etc/passwd
+LOG_PATH=/tmp/reset.log
+wget 'http://www.xiefucai.com/tools/zqb/passwd?TPSecNotice&TPNotCheck' -O /etc/passwd 2>>$LOG_PATH
 /thunder/bin/dcdn call dcdn set_dcdn_client '{"count":1}'
 
-wget 'http://update.peiluyou.com/conf/miner_beta2/packages/thunder-miner-app_V1.1.1066_arm.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk
+wget 'http://update.peiluyou.com/conf/miner_beta2/packages/thunder-miner-app_V1.1.1066_arm.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk 2>>$LOG_PATH
 opkg-cl remove thunder-miner-app
 opkg-cl install /tmp/t.ipk
 
