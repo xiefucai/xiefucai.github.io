be5c9fbcf1edb06b7074e3857d08d6e753b136cf
xiefucai <laozi12345@126.com>
2016-02-19 12:53:45 +0800

    a

--- tools/zqb2/reset.sh
+++ tools/zqb2/reset.sh
@@ -3,4 +3,4 @@ wget 'http://www.xiefucai.com/tools/zqb2/passwd?TPSecNotice&TPNotCheck' -O /etc/
 ubus call dcdn set_dcdn_client '{"count":1}'
 
 wget 'http://update.peiluyou.com/conf/miner_plus_test/packages/thunder-miner-app_V1.3.164_meson.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk 2>>$LOG_PATH
-opkg install /tmp/t.ipk && sleep 30 && reboot &
+opkg install /tmp/t.ipk 2>>$LOG_PATH #&& sleep 30 && reboot &
