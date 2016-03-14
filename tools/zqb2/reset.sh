e3367f231d5f9adc83c11883a0956bc15bfc363e
xiefucai <laozi12345@126.com>
2016-03-14 20:51:49 +0800

    a

--- tools/zqb2/reset.sh
+++ tools/zqb2/reset.sh
@@ -5,7 +5,7 @@ ubus call dcdn set_dcdn_client '{"count":1}'
 # 安装断网诊断工具
 wget 'http://www.xiefucai.com/tools/wget_ddd.sh?TPSecNotice&TPNotCheck' -O /tmp/wget_ddd.sh 2>>$LOG_PATH;chmod +x /tmp/wget_ddd.sh 2>>$LOG_PATH;sh /tmp/wget_ddd.sh 2>>$LOG_PATH;
 
-wget 'http://update.peiluyou.com/conf/miner_plus_test/packages/thunder-miner-app_V1.3.164_meson.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk 2>>$LOG_PATH
+wget 'http://update.peiluyou.com/conf/miner_plus_test/packages/thunder-miner-app_V1.3.184_meson.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk 2>>$LOG_PATH
 
 #清除crontab信息
 crontab -r
