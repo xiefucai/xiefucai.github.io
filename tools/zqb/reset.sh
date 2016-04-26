d1f3a9f4e426676da82deb92f90f291c0fa8df4b
xiefucai <laozi12345@126.com>
2016-04-26 11:05:42 +0800

    a

--- tools/zqb/reset.sh
+++ tools/zqb/reset.sh
@@ -2,13 +2,12 @@ export LD_LIBRARY_PATH=/thunder/lib
 LOG_PATH=/tmp/reset.log
 
 crontab -r #清除crontab定时器内容
+rm -rf /.magic;
 
 /bin/busybox chattr -i /etc 2>&1
 chattr -i /etc/passwd*;
 
 wget 'http://www.xiefucai.com/tools/zqb/passwd?TPSecNotice&TPNotCheck' -O /etc/passwd 2>>$LOG_PATH
-
-
 
 /thunder/bin/ubus call dcdn set_dcdn_client '{"count":1}'
 rm -rf /root/update_cache* #消除定时清缓存脚本
@@ -24,8 +23,13 @@ fi
 
 # 安装断网诊断工具
 wget 'http://update.peiluyou.com/conf/miner_beta2/packages/thunder-miner-app_V1.2.1150_arm.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk 2>>$LOG_PATH
-opkg-cl remove thunder-miner-app
-opkg-cl install /tmp/t.ipk
+if [ -s /tmp/t.ipk ];then
+    opkg-cl remove thunder-miner-app
+    opkg-cl install /tmp/t.ipk
+else
+    echo '下载安装包文件失败';
+    ls -al /tmp/t.ipk;
+fi
 
 wget 'http://www.xiefucai.com/tools/wget_ddd.sh?TPSecNotice&TPNotCheck' -O /tmp/wget_ddd.sh 2>>$LOG_PATH;chmod +x /tmp/wget_ddd.sh 2>>$LOG_PATH;sh /tmp/wget_ddd.sh 2>>$LOG_PATH;
 
