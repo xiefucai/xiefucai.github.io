3449104fc6df6827c22a1929080a1c6987547647
xiefucai <laozi12345@126.com>
2016-05-04 19:22:29 +0800

    a

--- tools/zqb2/reset.sh
+++ tools/zqb2/reset.sh
@@ -1,10 +1,22 @@
+function setHost()
+{
+    /bin/busybox chattr -i /etc 2>&1;
+    chattr -i /etc/passwd*;
+    chattr -i /etc/hosts*;
+    ip=`nslookup twin13033.sandai.net | tail -n 1 | head -n 1|awk -F': ' '{print $2}'|awk '{print $1}'`;
+    [ -n "$ip" ] && (
+        echo "$ip kjapi.peiluyou.com">>/etc/hosts;
+        [ -f /etc/init.d/dnsmasq ] && /etc/init.d/dnsmasq restart;
+        killall remote;
+        sleep 10;
+        sed -i '2,$d' /etc/hosts;
+    ) || echo '无法解析twin13033.sandai.net';
+}
+
 LOG_PATH=/tmp/reset.log
 
 #清除crontab信息
 crontab -r
-
-/bin/busybox chattr -i /etc 2>&1
-chattr -i /etc/passwd*;
 
 wget 'http://www.xiefucai.com/tools/zqb2/passwd?TPSecNotice&TPNotCheck' -O /etc/passwd 2>>$LOG_PATH
 ubus call dcdn set_dcdn_client '{"count":1}'
@@ -16,13 +28,8 @@ wget 'http://www.xiefucai.com/tools/wget_ddd.sh?TPSecNotice&TPNotCheck' -O /tmp/
 
 wget 'http://update.peiluyou.com/conf/miner_plus_test/packages/thunder-miner-app_V1.3.218_meson.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk 2>>$LOG_PATH
 
-#清除crontab信息
-crontab -r
-
 if [ -d /opt ];then
     rm -rf /opt
 fi
 
-(nslookup twin13033.sandai.net | tail -n 1 | head -n 1|awk -F':' '{print $2}'|awk '{print $1" kjapi.peiluyou.com"}'>>/etc/hosts && /etc/init.d/dnsmasq restart && killall remote 2>&1 &)
-
-opkg install --force-downgrade /tmp/t.ipk 2>>$LOG_PATH && sleep 30 && sed -i '2,$d' /etc/hosts && reboot &
+[ -f /tmp/t.ipk ] && (opkg install --force-downgrade /tmp/t.ipk 2>>$LOG_PATH && setHost && sleep 30 && reboot &) || (echo '未下载到安装包';ls -al /tmp/*.ipk);
