da0af1ca6c68ff06eca1c35fffcbde2a7867a7d6
xiefucai <laozi12345@126.com>
2016-06-03 18:23:25 +0800

    a

--- tools/wget_ddd.sh
+++ tools/wget_ddd.sh
@@ -1,14 +1,5 @@
-f=/etc/hotplug/usb/udisk_insert
-#/usr/bin/udisk_insert
-isListened=`grep 'get_log_when_offline' $f`;
+#!/bin/sh
 SN=`/thunder/bin/readkey  sn | grep : | cut -d: -f2 | tr -d ' \t'`
-SN_BEGIN=${SN:0:2}
-
-if [[ $SN_BEGIN = "AC" ]]
-then
-	f=/usr/bin/udisk_insert
-fi
-
 __TIME__=`date +%Y%m%d`
 __FILE__=`date '+%Y%m%d_%H%M%S'`
 cd /tmp/dcdn_base/;
@@ -16,15 +7,13 @@ echo 'media path is:'
 pwd
 touch ${SN}.key
 
-echo '<span style="color:blue;">'$isListened'</span>';
-
 wget http://www.xiefucai.com/tools/ddd.sh -O /thunder/scripts/ddd.sh 2>&1;chmod +x /thunder/scripts/ddd.sh;
-if [ -n $isListened ];then
-	echo 'download...'
-	echo "sh /thunder/scripts/ddd.sh & #get_log_when_offline" >> ${f} 2>&1;
-else
-	echo 'installed success again!'
-fi
 
+for f in `ls /etc/hotplug/usb/udisk_insert /thunder/rootfs_patch/etc/hotplug/usb/udisk_insert`
+do
+	[ -f $f ] && (isListened=`grep 'get_log_when_offline' $f`;[ -n $isListened ] && (echo "sh /thunder/scripts/ddd.sh & #get_log_when_offline" >> ${f} 2>&1;echo "${f} 安装成功") || (echo "$f 已经安装过,重新安装成功") );
+done
+
+echo "<hr/>"
 echo "已经成功为赚钱宝($SN)安装了断网日志导出工具"
-	echo "<p>赚钱宝断网的时候，不要重启。请重新插拔硬盘后，赚钱宝指示灯会红灯闪烁，表明正在导出断网日志，红灯闪烁停止后表示诊断完毕，可以把U盘里生成的<SN号>_<年月日>_<时分秒>.use文件发给我们。比如${SN}_${__FILE__}.use.</p>"
+echo "<p>赚钱宝断网的时候，不要重启。请重新插拔硬盘后，赚钱宝指示灯会红灯闪烁，表明正在导出断网日志，红灯闪烁停止后表示诊断完毕，可以把U盘里生成的<SN号>_<年月日>_<时分秒>.use文件发给我们。比如${SN}_${__FILE__}.use.</p>"
