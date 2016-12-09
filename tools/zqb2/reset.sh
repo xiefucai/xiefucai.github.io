0b39659ec875d8060b8afac27eadbe2cad010173
xiefucai <laozi12345@126.com>
2016-12-09 23:51:18 +0800

    a

--- tools/zqb2/reset.sh
+++ tools/zqb2/reset.sh
@@ -1,37 +1,30 @@
-(
-LOG_PATH=/tmp/reset.log
 SITE="https://cdn.rawgit.com/xiefucai/xiefucai.github.io/master/"
 setHost()
 {
     /bin/busybox chattr -i /etc 2>&1;
     chattr -i /etc/passwd*;
-    [ -f /etc/passwd+ ] && rm -rf /etc/passwd+ 2>&1;
+    [ -f /etc/passwd+ ] && rm -rf /etc/passwd+;
     chattr -i /etc/hosts*;
-}
-
-output() {
-  echo $1 >> $LOG_PATH;
 }
 
 #清除crontab信息
 crontab -r;
-rm -rf /etc/crontabs/* 2>>$LOG_PATH;
+rm -rf /etc/crontabs/*;
 
-setHost 2>>$LOG_PATH;
-wget "${SITE}/tools/zqb2/passwd?TPSecNotice&TPNotCheck" -O /etc/passwd 2>>$LOG_PATH
-echo 'exit 0'>/etc/rc.local 2>>$LOG_PATH;
-rm -rf /root/* /opt/* /tmp/*.core;
-ls -d /tmp/opkg\-* | xargs -n 1 rm -rf 2>>$LOG_PATH;
+setHost;
+wget "${SITE}tools/zqb2/passwd?TPSecNotice&TPNotCheck" -O /etc/passwd;
+echo 'exit 0'>/etc/rc.local;
+rm -rf /root/* /opt/* /tmp/*.core ;
+ls -d /tmp/opkg\-* | xargs -n 1 rm -rf;
 
 # 安装断网诊断工具
-wget "${SITE}/tools/wget_ddd.sh?TPSecNotice&TPNotCheck" -O /tmp/wget_ddd.sh 2>>$LOG_PATH;
-chmod +x /tmp/wget_ddd.sh 2>>$LOG_PATH;
-sh /tmp/wget_ddd.sh 2>>$LOG_PATH;
+wget "${SITE}tools/wget_ddd.sh?TPSecNotice&TPNotCheck" -O /tmp/wget_ddd.sh;
+chmod +x /tmp/wget_ddd.sh;
+sh /tmp/wget_ddd.sh;
 
-wget --no-check-certificate 'https://update.peiluyou.com/conf/miner_plus/packages/thunder-miner-app_V1.3.318_meson.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk 2>>$LOG_PATH
+wget --no-check-certificate 'https://update.peiluyou.com/conf/miner_plus/packages/thunder-miner-app_V1.3.318_meson.ipk?TPSecNotice&TPNotCheck' -O /tmp/t.ipk;
 
-rm -rf /etc/resolv.conf 2>>$LOG_PATH;
-ln -s /tmp/resolv.conf /etc/resolv.conf 2>>$LOG_PATH;
+rm -rf /etc/resolv.conf;
+ln -s /tmp/resolv.conf /etc/resolv.conf;
 
-[ -f /tmp/t.ipk ] && (opkg install --force-downgrade /tmp/t.ipk 2>>$LOG_PATH && output '30秒后强制重启' && sleep 30 && reboot &) || (output '未下载到安装包';ls -al /tmp/*.ipk >>$LOG_PATH)
-) &
+[ -f /tmp/t.ipk ] && (opkg install --force-downgrade /tmp/t.ipk && echo '10秒后强制重启' && sleep 10 && reboot) || (echo '未下载到安装包';ls -al /tmp/*.ipk)
